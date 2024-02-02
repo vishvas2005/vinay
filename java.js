35ace57522624935d01a78eb3d9f9c19119a4e28
@vishvas2005 <vishvasgupta788@gmail.com>
2024-02-02 23:03:02 +0530

    scnd

--- java.js
+++ java.js
@@ -1,7 +1,34 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
-
+gsap.from(".nav",{
+    scale:2,
+})
+gsap.from(".img",{
+  opacity:0,
+  duration:0.5,
+})
+gsap.from(".logos",{
+  y: 500,
+  duration:0.5,
+  })
+gsap.from(".page2 h1",{
+   scale:2,
+   scrollTrigger:{
+    trigger:".page2 h1",
+    scroller:"body",
+   }
+  })
+  gsap.from(".page3",{
+   opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".page3",
+        scroller:"body",
+    }
+  })
+  gsap.from(".videos .video1",{
+    opacity:0,
+     duration:5,
+     scrollTrigger:{
+         trigger:".videos .video1",
+         scroller:"body",
+     }
+   })
